d7626ea8bd96efddade9ca44265282e17dc9a5b5
IsabelQuintero07 <68484017+IsabelQuintero07@users.noreply.github.com>
2020-07-18 20:27:03 -0500

    Add files via upload

---
+++ NumeroPrimo/NumeroPrimo/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NumeroPrimo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbox = new System.Windows.Forms.ListBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbox
+            // 
+            this.lbox.FormattingEnabled = true;
+            this.lbox.Location = new System.Drawing.Point(99, 100);
+            this.lbox.Name = "lbox";
+            this.lbox.Size = new System.Drawing.Size(280, 225);
+            this.lbox.TabIndex = 0;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(130, 351);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(83, 29);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(256, 351);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(89, 29);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese un Número";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(279, 55);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.lbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbox;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnumero;
+    }
+}
+
---
+++ NumeroPrimo/NumeroPrimo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*IDENTACION:     El código deberá tener sangría, llaves, bloques de código 
+                  para una mejor legibilidad*/
+/*COMENTARIOS:    Son una parte fundamental de un buen código fuente, 
+                  con ellos se puede explicar esa parte del código que se ha escrito,
+                  ayudará en un futuro a un compañero que tenga que revisar el código a 
+                  entender más fácilmente lo que se quería conseguir.*/
+
+namespace NumeroPrimo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //numero = numero a ingresar
+            //n = Contador
+            //
+
+            int n, i, numero, num = 0;
+            numero = Convert.ToInt32(txtnumero.Text);
+            n = 2;
+            while (n <= numero)
+            {
+                for (i = 2; n % i != 0; i++) ;
+                if (i == n)
+                {
+                    lbox.Items.Add(n);
+                    if (++num % 10 == 0) { 
+                        lbox.Items.Add(n);
+                    }
+                }
+                n++;
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtnumero.Text = "";
+            lbox.Items.Clear();
+        }
+    }
+}
